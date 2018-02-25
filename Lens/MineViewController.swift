2e3b40f03cb4d9c7dc540ee31e90e766fd98617b
Archie Yu <yuyq96@126.com>
2018-02-25 16:04:12 +0800

    Add Mine table cells, modify tab item text offset

---
+++ Lens/MineViewController.swift
@@ -0,0 +1,132 @@
+//
+//  MineViewController.swift
+//  Lens
+//
+//  Created by Archie Yu on 2018/2/25.
+//  Copyright © 2018年 Archie Yu. All rights reserved.
+//
+
+import UIKit
+
+class MineViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        switch(section) {
+            case 0: return 1
+            case 1: return 3
+            case 2: return 2
+            default: return 0
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MineCell", for: indexPath)
+        
+//        cell.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.1)
+//        cell.textLabel?.textColor = .white
+        
+        cell.accessoryType = .disclosureIndicator
+        switch indexPath.section {
+        case 0:
+            switch indexPath.row {
+            case 0:
+                cell.textLabel?.text = "Riach"
+            default:
+                break
+            }
+        case 1:
+            switch indexPath.row {
+            case 0:
+                cell.textLabel?.text = "My Equipment"
+            case 1:
+                cell.textLabel?.text = "Wishlist"
+            case 2:
+                cell.textLabel?.text = "Budget"
+            default:
+                break
+            }
+        case 2:
+            switch indexPath.row {
+            case 0:
+                cell.textLabel?.text = "Settings"
+            case 1:
+                cell.textLabel?.text = "About"
+            default:
+                break
+            }
+        default:
+            break
+        }
+        return cell
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
